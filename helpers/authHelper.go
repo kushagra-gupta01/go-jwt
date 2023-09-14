f05b697e7ee42d60117689b5a23ff7110349cd36
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-09-14 16:56:51 +0530

    authHelper added

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,31 @@
 package helpers
+
+import(
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context,role string)(err error){
+	userType := c.GetString("user_type")
+	err=nil
+
+	if userType !=role{
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(ctx gin.Context, userId string)(err error){
+	userType := ctx.GetString("user_type")
+	uid := ctx.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid !=userId{
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+
+	err = CheckUserType(ctx,userType)
+	return err
+}
