baada05b825a750464c5000b6e38e941e327e205
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-09-16 22:03:45 +0530

    middleware added

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,2 +1,33 @@
 package middleware
 
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/kushagra-gupta01/go-jwt/helpers"
+)
+
+func Authenticate()gin.HandlerFunc{
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":fmt.Sprintf("No authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims,err := helpers.ValidateToken(clientToken)
+		if err != ""{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":err})
+			c.Abort()
+			return
+		}
+		c.Set("email",claims.Email)
+		c.Set("first_name",claims.First_Name)
+		c.Set("last_name",claims.Last_Name)
+		c.Set("uid",claims.User_type)
+		c.Set("user_type",claims.User_type)
+		c.Next()
+	}
+}
+
